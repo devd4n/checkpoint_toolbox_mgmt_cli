752a67a19f245495430ed6d2940fb7645138ae7f
devd4n <daniel-schmitt-it@gmx.de>
2021-10-17 11:38:17 +0200

    add logging and exception handling to draft 0.0.6

--- mgmt_cli_toolbox.py
+++ mgmt_cli_toolbox.py
@@ -18,7 +18,7 @@
     Author: https://github.com/devd4n
 """
 
-#### DRAFT VERSION 0.0.4
+#### DRAFT VERSION 0.0.6
 
 MANUAL = """
 [./mgmt_cli_toolbox.py | mgmt_cli_toolbox.py] [-i <mgmt_cli_session_id> | -s mgmt_cli_session_file] [COMMAND] [PARAMS]
@@ -38,6 +38,7 @@ import time
 import os
 import json
 import sys, getopt
+import datetime
 
 json_db = ["""
 { "objects" : [ {
@@ -191,8 +192,10 @@ json_db = ["""
 #THREAT_POLICIES=["", ""]
 #NAT_PACKAGES=["", ""]
 RELEVANT_OBJECTS=["TCP_SERVICE"]
-PATH_LOG_FILE="mgmt_cli_toolbox.log" # Define Log File Location if "" => it is written to stdout
-
+PATH_ROOT = "./toolbox_files/"
+DATABASE_STORE_PATH = PATH_ROOT + "db.json"
+LOG_FILE= PATH_ROOT + "" # Define Log File Location if "" => it is written to stdout
+LOG_LEVEL="DEBUG"
 """
 ########################################################################
 #------------------------ STATIC PARAMETERS -------------------------------------#
@@ -210,42 +213,97 @@ OBJ_TYPES = {
     #"THREAT_PREV_RULE" : { "rep" : "host", "cli_get", "hosts", "cli_set", "host" },
     #"NAT_RULE" : { "rep" : "host", "cli_get", "hosts", "cli_set", "host" },
 }
-SYNTAX_SHOW = {}
+LOG_LVL = {
+    "FATAL" : 1,
+    "ERROR" : 2,
+    "WARNING" : 3,
+    "INFO" : 7,
+    "DEBUG" : 8,
+    "TRACE" : 9,
+}
+LOG_LVL_INV = {
+    1 : "FATAL",
+    2 : "ERROR",
+    3 : "WARNING",
+    7 : "INFO",
+    8 : "DEBUG",
+    9 : "TRACE",
+}
+GLOBAL_STORAGE_DICT = {}
 
 """
 ########################################################################
-#------------------------ METHODS -------------------------------------#
+#---------------------- API METHODS -----------------------------------#
+########################################################################
+"""
+
+def load_all ():
+    log("################################################################")
+    log("# load_all (" + "" + "," + "" + ")")
+    log("################################################################")
+    var_data = {}
+    for var_type in RELEVANT_OBJECTS:
+        if var_type == "ACCESS_RULE":
+            log("load Access Policy...")
+        elif var_type == "THREAT_RULE":
+            log("load Threat Policy...")
+            #for var_policy in THREAT_POLICIES:
+            #    var_data += get_all_data_of_type(TYPE_THREAT_RULE, "\"" + var_policy + "\"")
+        else:
+            log("load Objects of Type:" + var_type + "...")
+            var_data = merge_dicts(var_data, get_all_data_of_type(var_type, ""))
+    GLOBAL_STORAGE_DICT["by_uid"] = var_data
+    save()
+
+def uuid_where_used (p_uuid):
+    log("################################################################")
+    log("# uuid_where_used (" + "" + "," + "" + ")")
+    log("################################################################")
+    load_all()
+    #combined_var_data = get_all_data_of_type(OBJ_TYPES["SERVICE_GROUP"]["cli_show"])
+    log("combined_var_data with uids as keys and used by params" + str(GLOBAL_STORAGE_DICT))
+    # Retrieve all used_by uuids for the given uid
+    try:
+        used_by_list = GLOBAL_STORAGE_DICT["by_uid"][p_uuid]["used_by"]
+        return used_by_list
+    except KeyError:
+        log("invalid uuid - uid" + str(p_uuid) + " can't be found in data store", LOG_LVL["ERROR"])
+
+
+
+"""
+########################################################################
+#---------------------- PRIVATE METHODS -------------------------------#
 ########################################################################
 """
 def get_all_data_of_type (p_req_type, p_rulestring=""):
   log("################################################################")
   log("# get_all_data_of_type (" + p_req_type + "," + p_rulestring + ")")
   log("################################################################")
-  #$rulestring = ""    # This variable is only needed for Rule specific requests
+  #rulestring = ""    # This variable is only needed for Rule specific requests
   var_offset = 0
   var_last_item_index = 0
-  var_data = ""
-  # get number of Objects of the given req_type
-    #run_mgmt_cli(session_uid, p_req_type + " " + p_rulestring + " limit 1 ", "")
-  var_mgmt_feedback = json.loads(json_db[0])
-  var_object_count = var_mgmt_feedback['total']
-  # make mgmt_cli show commands es much as needed
+  #run_mgmt_cli(session_uid, p_req_type + " " + p_rulestring + " limit 1 ", "")
+  var_object_count = json.loads(json_db[0])['total']
   var_data_dict = { "objects" : []}
   var_count_requests = 0
+  # how much requests are needed to retrieve all data:
+  # var_object_count/
+  # make mgmt_cli show commands es much as needed
   while (var_last_item_index <= var_object_count):
     log("mgmt_cli show "+ p_req_type + " " + p_rulestring + "details-level full limit 1 offset " + str(var_offset) + " --session-id " + "session_id")
     var_json_data = json_db[var_count_requests]
-    #var_data += run_bash("echo request " + str(MAX_OBJECT_PER_REQUEST) + " data-sets") #mgmt_cli show $req_type $rulestring details-level full limit MAX_OBJECT_PER_REQUEST offset $offset -s $mgmt_cli_session_file)
+    #mgmt_cli show $req_type $rulestring details-level full limit MAX_OBJECT_PER_REQUEST offset $offset -s $mgmt_cli_session_file)
     var_data_dict_tmp = json.loads(var_json_data)
-    log("keys" + str(var_data_dict_tmp.keys()))
+    log("keys_retrieved via cli: " + str(var_data_dict_tmp.keys()), LOG_LVL["DEBUG"])
     #log("var_data_dict_tmp " + str(var_data_dict_tmp))
     for obj in var_data_dict_tmp['objects']:
         var_data_dict['objects'].append(obj) # Hint Duplicates can be occur - no errors [[FIX]]
     var_offset += MAX_OBJECT_PER_REQUEST
     var_last_item_index = var_last_item_index + MAX_OBJECT_PER_REQUEST
     var_count_requests += 1
-  log("announced data count: " + str(var_object_count))
-  log("retrieved data count: " + str(len(var_data_dict['objects'])))
+  log("announced data count: " + str(var_object_count), LOG_LVL["DEBUG"])
+  log("retrieved data count: " + str(len(var_data_dict['objects'])), LOG_LVL["DEBUG"])
   var_data_dict = parse_obj_to_uid_dict(var_data_dict, p_req_type)
   var_data_dict = add_used_by(var_data_dict)
   return var_data_dict
@@ -269,45 +327,16 @@ def parse_obj_to_uid_dict(p_dict, p_type):
 
 def add_used_by (p_data_dir):
     log("################################################################")
-    log("# add_used_by (" + "" + "," + "" + ")")
+    log("# add_used_by (" + "p_data_dir" + "," + "" + ")")
     log("################################################################")
     var_data = p_data_dir
     for i_key in p_data_dir:
         var_data[i_key]["used_by"] = []
         for i_key_sub in p_data_dir:
-            var_string = var_data[i_key_sub]["as_string"]
-            if i_key in var_string:
-                if not i_key is i_key_sub:
-                    var_data[i_key]["used_by"].append(i_key_sub)
-    log(var_data)
-    return var_data
-
-def uuid_where_used (p_uuid):
-    log("################################################################")
-    log("# uuid_where_used (" + "" + "," + "" + ")")
-    log("################################################################")
-    combined_var_data = load_all_policy_objects()
-    #combined_var_data = get_all_data_of_type(OBJ_TYPES["SERVICE_GROUP"]["cli_show"])
-    log(combined_var_data)
-    # Retrieve all used_by uuids for the given uid
-    used_by_list = combined_var_data[p_uuid]["used_by"]
-    return used_by_list
-
-def load_all_policy_objects ():
-    log("################################################################")
-    log("# load_all_policy_objects (" + "" + "," + "" + ")")
-    log("################################################################")
-    var_data = {}
-    for var_type in RELEVANT_OBJECTS:
-        if var_type == "ACCESS_RULE":
-            log("load Access Policy...")
-        elif var_type == "THREAT_RULE":
-            log("load Threat Policy...")
-            #for var_policy in THREAT_POLICIES:
-            #    var_data += get_all_data_of_type(TYPE_THREAT_RULE, "\"" + var_policy + "\"")
-        else:
-            log("load Objects of Type:" + var_type + "...")
-            var_data = merge_dicts(var_data, get_all_data_of_type(var_type, ""))
+            if not i_key is i_key_sub:
+              var_string = var_data[i_key_sub]["as_string"]
+              if i_key in var_string:
+                var_data[i_key]["used_by"].append(i_key_sub)
     return var_data
 
 def object_by_uid (p_uid):
@@ -316,6 +345,12 @@ def object_by_uid (p_uid):
     log("################################################################")
     log("this function is not implemented yet")
 
+
+def save ():
+    save_data_to_file (DATABASE_STORE_PATH, GLOBAL_STORAGE_DICT)
+
+def load ():
+    GLOBAL_STORAGE_DICT = load_data_from_file()
 
 """
 ########################################################################
@@ -331,22 +366,41 @@ def run_bash (p_command):
 def run_mgmt_cli (p_session_uid, p_command, p_after_command):
     run_bash("mgmt_cli " + p_command + "--session-id " + p_session_uid + " " + p_after_command)
 
-def log (p_logstring):
-  if (PATH_LOG_FILE != ""):
-    f = open(PATH_LOG_FILE, "a")
-    f.write(str(p_logstring) + "\n")
-    f.close()
-  else:
-    print(p_logstring)
+def save_data_to_file (p_file_name, p_dict):
+ json.dump( p_dict, open( p_file_name, 'w' ) )
+
+def load_data_from_file (p_file_name):
+ json.load( open(p_file_name) )
+
+def log (p_logstring, p_LOG_LVL=9):
+  if p_LOG_LVL <= LOG_LVL[LOG_LEVEL]:
+      p_logstring = str(get_timestamp()) + "  |" + str(LOG_LVL_INV[p_LOG_LVL]) + ("|  ") + str(p_logstring)
+      if (LOG_FILE != "" and LOG_FILE != PATH_ROOT):
+          f = open(LOG_FILE, "a")
+          f.write(p_logstring + "\n")
+          f.close()
+      else:
+        print(p_logstring)
 
 def clear_log ():
     ## If file exists, delete it ##
-    if os.path.isfile(PATH_LOG_FILE):
-        os.remove(PATH_LOG_FILE)
-    else:    ## Show an error ##
-        print("Error: %s file not found" % PATH_LOG_FILE)
+    if (LOG_FILE != ""  and LOG_FILE != PATH_ROOT):
+      if os.path.isfile(LOG_FILE):
+        os.remove(LOG_FILE)
+      else:    ## Show an error ##
+        log("couldn't delete log file > cause not exists on following path: " + LOG_FILE, LOG_LVL["WARNING"])
 
+def get_timestamp():
+  var_now = datetime.datetime.now()
+  var_timestamp = str(var_now.year) + str(var_now.month) + str(var_now.day) + "|" + str(var_now.hour) + ":" + str(var_now.minute) + ":" + str(var_now.second)
+  return var_timestamp
 
+def create_output_folder ():
+    if not os.path.isdir(PATH_ROOT):
+        log('The directory is not present. Creating a new one..')
+        os.mkdir(PATH_ROOT)
+    else:
+        log('The directory is present.')
 """
 ########################################################################
 #------------------------ PYTHON - HELPERS ----------------------------#
@@ -374,24 +428,34 @@ def main (argv):
   try:
     opts, args = getopt.getopt(argv,"hi:s:c:",['help', 'session_id=', 'session_file=', 'command='])
   except getopt.GetoptError:
+    log("The parameters and arguments aren't correct" + LOG_LVL["ERROR"])
     print(var_man_page)
     sys.exit(2)
-  for opt, arg in opts:
-    log("opt -> " + str(opt))
-    log("arg -> " + str(arg))
-    log("args -> " + str(args))
-    if opt in ('-h', "--help"):
-      print(var_man_page)
-      sys.exit()
-    if opt in ("-i", "--session_id"):
-      var_mgmt_cli_session_id = arg
-    #elif opt in ("-s", "--session_file"):
-    #  var_mgmt_cli_session_id = ""
-    if arg in COMMANDS:
-      clear_log()
-      log("deleted_old_log_file")
-      result = globals()[str(arg)](args[0])
-      print(result)
+  try:
+    create_output_folder()
+    for opt, arg in opts:
+      log("opt -> " + str(opt))
+      log("arg -> " + str(arg))
+      log("args -> " + str(args))
+      if opt in ('-h', "--help"):
+        print(var_man_page)
+        sys.exit()
+      if opt in ("-i", "--session_id"):
+        var_mgmt_cli_session_id = arg
+      #elif opt in ("-s", "--session_file"):
+      #  var_mgmt_cli_session_id = ""
+      if opt in ("-c", "--command"):
+        if arg in COMMANDS:
+          clear_log()
+          log("deleted_old_log_file")
+          result = globals()[str(arg)](args[0])
+          print(result)
+  except IndexError:
+      log("parameters and arguments are missing", LOG_LVL["ERROR"])
+  except BaseException as err:
+      log(str("FATAL ERROR: {0}".format(err)), LOG_LVL["FATAL"])
+      log("The Script crashed pls check manpage with -h option", LOG_LVL["FATAL"])
+      sys.exit(2)
 
 if __name__ == "__main__":
   main(sys.argv[1:])
